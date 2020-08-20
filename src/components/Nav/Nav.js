7dafe967b75e7ed1f486aafa476691a3841bd727
Ken Lin <kel117@pitt.edu>
2020-08-20 13:36:18 -0400

    Moved styling to external css

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,20 +3,10 @@ import "./Nav.css";
 
 function Nav() {
   return (
-    <div
-      style={{
-        height: "60px",
-        width: "100vw",
-        backgroundColor: "#222831",
-        position: "fixed",
-        top: "0px",
-        left: "0px",
-        zIndex: "10",
-      }}
-    >
+    <div className="nav">
       <div className="nav-container">
         <a className="nav-link" href="/">
-          Github Users
+          Github Users Search
         </a>
       </div>
     </div>
